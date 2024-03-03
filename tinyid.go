392273bc32fa0a4dad2c1e4a389340f72ff12079
Archishman Sengupta <archishman1808@gmail.com>
2024-03-03 21:30:17 +0530

    feat: generate tiny id function

--- tinyid.go
+++ tinyid.go
@@ -2,6 +2,10 @@ package tinyid
 
 import (
 	"crypto/rand"
+	"errors"
+	"math"
+	"math/bits"
+	"strings"
 )
 
 // Represent the default alphabet and size for the generated tiny IDs.
@@ -21,4 +25,43 @@ func randomBufferGenerator(step int) ([]byte, error) {
 	buffer := make([]byte, step)
 	_, err := rand.Read(buffer)
 	return buffer, err
+}
+
+// GenerateTinyId generates a random sttring based on the alphabet and size.
+func generateTinyId(alphabet string, size int) (string, error) {
+	if size <= 0 {
+		return "", errors.New("tinyId: size must be greater than 0")
+	}
+
+	// calculate mask to ensure that random bytes are mapped properly to the given alphabet
+	mask := 2<<uint32(31-bits.LeadingZeros32(uint32(len(alphabet)-1|1))) - 1
+
+	// calulates the step size based on the desired size and alphabet length to ensure an even distribution of characters
+	step := int(math.Ceil(1.6 * float64(mask*size) / float64(len(alphabet))))
+
+	// initialize string builder to efficiently generate the ID
+	id := new(strings.Builder)
+
+	id.Grow(size)
+
+	// iterating to generate the ID
+	for {
+		// generate a random buffer of bytes
+		randomBuffer, err := randomBufferGenerator(step)
+		if err != nil {
+			return "", err
+		}
+
+		for i := 0; i < step; i++ {
+			// mapping each byte to a character in the alphabet using bitwise operations and the calculated mask
+			currentIndex := int(randomBuffer[i]) & mask
+			if currentIndex < len(alphabet) {
+				if err := id.WriteByte(alphabet[currentIndex]); err != nil {
+					return "", err
+				} else if id.Len() == size {
+					return id.String(), nil
+				}
+			}
+		}
+	}
 }
